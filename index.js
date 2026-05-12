44cae5d83dd626bf28f4eacdcd053f61791cde74
azizshahid <azizshahid222@gmail.com>
2026-05-12 21:33:47 +0530

    added the delete feature in the To-do app

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const buttonEl = document.querySelector("#button");
 const todoList = document.querySelector("#todo_list");
 
 function addTodo() {
-    const li = document.createElement("li");
-    let inputValue = document.querySelector(".input").value;
-    const t = document.createTextNode(inputValue);
-    li.appendChild(t);
+  let inputValue = document.querySelector(".input").value;
 
-    // Cheack input validation
-    if (inputValue === "") {
-        alert("Please enter a valid value");
-    } else {
-        todoList.appendChild(li);
-    }
-    document.querySelector("input").value = "";
+  // Cheack input validation
+  if (inputValue === "") {
+    alert("Please enter a valid value");
+  } else {
+    const btnEl = document.createElement("button");
+    const li = document.createElement("li");
+    const t = document.createTextNode(inputValue);
+    btnEl.innerText = "Delete";
+    li.appendChild(t);
+    li.appendChild(btnEl)
+    todoList.appendChild(li);
+    btnEl.addEventListener("click", function (e) {
+      console.log(event.target.parentElement.remove());
+    });
+  }
+  document.querySelector("input").value = "";
 }
 
 buttonEl.addEventListener("click", addTodo);
